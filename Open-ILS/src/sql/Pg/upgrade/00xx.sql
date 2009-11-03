6979692d5a4209cb14e80ac9c2864c5fbf6fb17e
Joe Atzberger <atz@esilibrary.com>
2009-11-03 18:32:57 +0000

    Datatype is "string" not "text"

--- Open-ILS/src/sql/Pg/upgrade/00xx.sql
+++ Open-ILS/src/sql/Pg/upgrade/00xx.sql
@@ -7,7 +7,7 @@ INSERT INTO config.org_unit_setting_type ( name, label, description, datatype )
 ( 'notice.telephony.enabled',
   'Enable or disable telephony',
   'Select "SIP" or "PSTN" to enable telephony notices over the respective carriers.  If "Off", no telephony is used.  If "PSTN", then notice.telphony.channels_available must be specified.',
-  'text' ),
+  'string' ),
 
 ('notice.telephony.channels_available', 'PSTN channels to use for outgoing calls', 'Channels will be used in the order listed.', 'array'),
 
@@ -15,12 +15,12 @@ INSERT INTO config.org_unit_setting_type ( name, label, description, datatype )
     'Note: This is not the channel value itself.  Index is zero-based.', 'integer'),
 
 ('notice.telephony.callfile_lines',     'Arbitrary line(s) to include in each notice callfile',
-    'Line(s) must be valid for your target server and platform (e.g. Asterisk 1.4).', 'text'),
+    'Line(s) must be valid for your target server and platform (e.g. Asterisk 1.4).', 'string'),
 
-('notice.telephony.host.hostname', 'Telephony: hostname or IP', 'Specify the server that will make the calls', 'text'),
+('notice.telephony.host.hostname', 'Telephony: hostname or IP', 'Specify the server that will make the calls', 'string'),
 ('notice.telephony.host.port',     'Telephony: port number',    'Server port where we find the listening agent. Default: 10080', 'integer'),
-('notice.telephony.host.username', 'Telephony: username',       'Required by listening agent.', 'text'),
-('notice.telephony.host.password', 'Telephony: password',       'Required by listening agent.', 'text');
+('notice.telephony.host.username', 'Telephony: username',       'Required by listening agent.', 'string'),
+('notice.telephony.host.password', 'Telephony: password',       'Required by listening agent.', 'string');
 
 COMMIT;
 
