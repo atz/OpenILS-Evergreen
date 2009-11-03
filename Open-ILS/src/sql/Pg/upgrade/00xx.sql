f3aeb37c2642cfd724ae04a2ea50f276034e7218
Joe Atzberger <atz@esilibrary.com>
2009-11-03 16:53:39 +0000

    Most of the SQL to add new Telephony settings.

--- Open-ILS/src/sql/Pg/upgrade/00xx.sql
+++ Open-ILS/src/sql/Pg/upgrade/00xx.sql
@@ -6,8 +6,21 @@ INSERT INTO config.org_unit_setting_type ( name, label, description, datatype )
     VALUES 
 ( 'notice.telephony.enabled',
   'Enable or disable telephony',
-  'Select "SIP" or "PTSN" to enable telephony features over the respective carriers.  If "Off", no telephony is used.  If "PTSN", then notice.telphony.channels_available must be specified.',
+  'Select "SIP" or "PSTN" to enable telephony notices over the respective carriers.  If "Off", no telephony is used.  If "PSTN", then notice.telphony.channels_available must be specified.',
   'text' ),
+
+('notice.telephony.channels_available', 'PSTN channels to use for outgoing calls', 'Channels will be used in the order listed.', 'array'),
+
+('notice.telephony.channel_last_used',  'Index of the channel last used from channels_available',
+    'Note: This is not the channel value itself.  Index is zero-based.', 'integer'),
+
+('notice.telephony.callfile_lines',     'Arbitrary line(s) to include in each notice callfile',
+    'Line(s) must be valid for your target server and platform (e.g. Asterisk 1.4).', 'text'),
+
+('notice.telephony.host.hostname', 'Telephony: hostname or IP', 'Specify the server that will make the calls', 'text'),
+('notice.telephony.host.port',     'Telephony: port number',    'Server port where we find the listening agent. Default: 10080', 'integer'),
+('notice.telephony.host.username', 'Telephony: username',       'Required by listening agent.', 'text'),
+('notice.telephony.host.password', 'Telephony: password',       'Required by listening agent.', 'text')
 
 COMMIT;
 
