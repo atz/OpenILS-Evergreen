36ce765a900def6f8a872a8b4af45176a27e8f85
Joe Atzberger <atz@esilibrary.com>
2009-11-12 22:36:45 +0000

    Command line RPC::XML::Client test for asterisk mediator

---
+++ Open-ILS/asterisk/injector/test_client.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+#
+
+use warnings;
+use strict;
+
+use RPC::XML::Client;
+use Data::Dumper;
+
+my $host = @ARGV ? shift : 'http://localhost';
+$host =~ /^\S+:\/\// or $host  = 'http://' . $host;
+$host =~ /:\d+$/     or $host .= ':10080';
+print "Trying host: $host\n";
+
+my $client = new RPC::XML::Client($host);
+# TODO: add scheduling intelligence and use it here.
+
+print "Sending 'retrieve' request\n";
+my $resp = $client->send_request('retrieve');
+
+print Dumper($resp);
+
