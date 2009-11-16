ea9f94076c994e21560778eba156e8cbb6a6e895
Joe Atzberger <atz@esilibrary.com>
2009-11-16 16:09:08 +0000

    Abstractify test client, allow arbitrary commands to be passed.

--- Open-ILS/asterisk/injector/test_client.pl
+++ Open-ILS/asterisk/injector/test_client.pl
@@ -4,23 +4,39 @@
 use warnings;
 use strict;
 
+use Getopt::Long;
 use RPC::XML::Client;
 use Data::Dumper;
 
-my $host = @ARGV ? shift : 'http://localhost';
+# DEFAULTS
+my $host = 'http://localhost';
+my $verbose = 0;
+
+GetOptions(
+    'host=s'  => \$host,
+    'verbose' => \$verbose,
+);
+
+# CLEANUP
 $host =~ /^\S+:\/\// or $host  = 'http://' . $host;
 $host =~ /:\d+$/     or $host .= ':10080';
+
+# MAIN
 print "Trying host: $host\n";
 
 my $client = new RPC::XML::Client($host);
-# TODO: add scheduling intelligence and use it here.
 
-print "Sending 'retrieve' request\n";
-my $resp = $client->send_request('retrieve');
+my @commands = @ARGV;
+scalar(@commands) or push @commands, 'retrieve';    # default
+
+print "Sending request: ", join(' ', @commands), "\n";
+my $resp = $client->send_request(@commands);
 
 if (ref $resp) {
     print "Return is " . ref($resp), "\n";
-
+    my $code = $resp->{code};
+    # print "Code: ", ($resp->{code}->as_string || 'UNKNOWN'), "\n";
+    print "Code: ", ($code->value || 'UNKNOWN'), "\n";
 }
-print Dumper($resp);
+$verbose and print Dumper($resp);
 
