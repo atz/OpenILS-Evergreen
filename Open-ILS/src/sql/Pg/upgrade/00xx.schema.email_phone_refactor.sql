76fa7ca46e4c7de14e4996e3892c0095d9a9b6cd
Joe Atzberger <atz@esilibrary.com>
2009-11-19 22:10:06 +0000

    Utilize new atomized procedure for rebuilding view.

--- Open-ILS/src/sql/Pg/upgrade/00xx.schema.email_phone_refactor.sql
+++ Open-ILS/src/sql/Pg/upgrade/00xx.schema.email_phone_refactor.sql
@@ -56,6 +56,7 @@ ALTER TABLE auditor.actor_usr_history
     ADD COLUMN evening_phone_id INT,
     ADD COLUMN   other_phone_id INT;
 
+
 -- now the data moving: same kind of operations, 3 times.
 -- We do NOT add the number a new entry for each type where it is populated
 -- if the number is the same digits.
@@ -89,17 +90,26 @@ UPDATE actor.usr SET   other_phone_id = actor.usr_phone.id FROM actor.usr_phone
     WHERE actor.usr_phone.usr    = actor.usr.id
      AND  actor.usr_phone.digits = actor.usr.other_phone;
 
--- TODO: retaylor CONSTRAINTS
+-- TODO: retaylor any remaining CONSTRAINTS?
+
+DROP VIEW IF EXISTS auditor.actor_usr_lifecycle;
 
 ALTER TABLE actor.usr
-    DROP COLUMN     day_phone,
-    DROP COLUMN evening_phone,
-    DROP COLUMN   other_phone;
+    DROP COLUMN     day_phone CASCADE,
+    DROP COLUMN evening_phone CASCADE,
+    DROP COLUMN   other_phone CASCADE;
 
 ALTER TABLE auditor.actor_usr_history
-    RENAME COLUMN     day_phone TO     day_phone_val,
-    RENAME COLUMN evening_phone TO evening_phone_val,
-    RENAME COLUMN   other_phone TO   other_phone_val;
+    DROP COLUMN     day_phone CASCADE,
+    DROP COLUMN evening_phone CASCADE,
+    DROP COLUMN   other_phone CASCADE;
+
+---- Can't do RENAME's because the auditor VIEW requires a set number of columns for UNION ALL
+--    RENAME COLUMN     day_phone TO     day_phone_val,
+--    RENAME COLUMN evening_phone TO evening_phone_val,
+--    RENAME COLUMN   other_phone TO   other_phone_val;
+
+SELECT auditor.create_auditor_lifecycle ('actor', 'usr');   -- This rebuilds the view we dropped.
 
 COMMIT;
 
