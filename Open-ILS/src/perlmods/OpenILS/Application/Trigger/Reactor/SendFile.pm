f070cd083746c07e588c07561f004bd1a08393e1
Joe Atzberger <atz@esilibrary.com>
2009-12-18 18:00:53 +0000

    Fix package name and params depth
    Added more docs also.

--- Open-ILS/src/perlmods/OpenILS/Application/Trigger/Reactor/SendFile.pm
+++ Open-ILS/src/perlmods/OpenILS/Application/Trigger/Reactor/SendFile.pm
@@ -1,4 +1,4 @@
-package   OpenILS::Application::Trigger::Reactor::SendEmail;
+package   OpenILS::Application::Trigger::Reactor::SendFile;
 use       OpenILS::Application::Trigger::Reactor;
 use base 'OpenILS::Application::Trigger::Reactor';
 
@@ -17,13 +17,22 @@ use warnings;
 sub ABOUT {
     return <<ABOUT;
 
-The SendFile Reactor Module attempts to transfer a file to a remote host.
+The SendFile Reactor Module attempts to transfer a file to a remote server.
 Net::uFTP is used, encapsulating the available options of SCP, FTP and SFTP.
 
 No default template is assumed, and all information is expected to be gathered
-by the Event Definition through either Environment or Parameter definitions.
+by the Event Definition through event parameters:
+   ~ remote_host (required)
+   ~ remote_user
+   ~ remote_password
+   ~ remote_account
+   ~ type (FTP, SFTP or SCP -- default FTP)
+   ~ port
+   ~ debug
 
-Note: none of the remote_X arguments are actually required, except remote_host.
+The latter three are optionally passed to the Net::uFTP constructor.
+
+Note: none of the parameters are actually required, except remote_host.
 That is because remote_user, remote_password and remote_account can all be 
 extrapolated from other sources, as the Net::FTP docs describe:
 
@@ -35,14 +44,19 @@ extrapolated from other sources, as the Net::FTP docs describe:
     If no password is given and the login is anonymous then anonymous@
         will be used for password.
 
+Note that specifying a password will require you to specify a user.
+Similarly, specifying an account requires both user and password.
+That is, there are no assumed defaults when the latter arguments are used.
+
 ABOUT
 }
 
 sub handler {
     my $self = shift;
     my $env  = shift;
+    my $params = $env->{params};
 
-    my $host = $env->{remote_host};
+    my $host = $params->{remote_host};
     unless ($host) {
         $logger->error("No remote_host specified in env");
         return;
@@ -50,8 +64,7 @@ sub handler {
 
     my %options = ();
     foreach (qw/debug type port/) {
-        $env->{$_} or next;
-        $options{$_} = $env->{$_};
+        $options{$_} = $params->{$_} if $params->{$_};
     }
     my $ftp = Net::uFTP->new($host, %options);
 
@@ -65,7 +78,7 @@ sub handler {
 
     my @login_args = ();
     foreach (qw/remote_user remote_password remote_account/) {
-        push @login_args, $env->{$_} if $env->{$_};
+        push @login_args, $params->{$_} if $params->{$_};
     }
     unless ($ftp->login(@login_args)) {
         $logger->error("SendFile Reactor: failed login to $host w/ args(" . join(',', @login_args) . ")");
@@ -73,15 +86,14 @@ sub handler {
     }
 
     my @put_args = ($tmp);
-    push @put_args, $env->{remote_file} if $env->{remote_file};     # user can specify remote_file name, optionally
+    push @put_args, $params->{remote_file} if $params->{remote_file};     # user can specify remote_file name, optionally
     my $filename = $ftp->put(@put_args);
     if ($filename) {
         $logger->info("SendFile Reactor: successfully sent ${host} $filename");
         return 1;
     }
 
-    $text =~ s/\n/ /og;
-    $logger->error("SendFile Reactor: failed with error: $! for text $text");
+    $logger->error("SendFile Reactor: put to $host failed with error: $!");
     return;
 }
 
