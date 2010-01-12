dcfa1ad197f1745988a52d731b9d0e1b7e2fd152
Joe Atzberger <atz@esilibrary.com>
2010-01-12 00:02:05 +0000

    Raw test/debug version of edi_fetcher

---
+++ Open-ILS/src/support-scripts/edi_fetcher.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# ---------------------------------------------------------------
+# Copyright (C) 2010 Equinox Software, Inc
+# Author: Joe Atzberger <jatzberger@esilibrary.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ---------------------------------------------------------------
+
+use strict;
+use warnings;
+
+use OpenILS::Cronscript;
+use Data::Dumper;
+my $x = OpenILS::Cronscript->new({foo=>'bar', verbose=>-1, 'my_int=i'=>-1, 'lock-file'=>'/tmp/whatever'});
+print "in $0 pt 1: ", Dumper($x);
+
+$x->MyGetOptions({another_opt=>"another_val"});     # not yet implemented
+print "in $0 pt 2: ", Dumper($x);
+
+$x->bootstrap;
+print "done\n";
