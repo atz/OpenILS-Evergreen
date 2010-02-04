0388db07c999c116f77e9270dd1940070ec14f33
Joe Atzberger <atz@esilibrary.com>
2010-02-04 15:21:58 +0000

    take command-line ARGS, optionally

--- edi_scratch/ruby_edi/mbk_test.rb
+++ edi_scratch/ruby_edi/mbk_test.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
 
-# remember to export RUBYLIB=path/to/jedi_stuff
+#    remember to export RUBYLIB=path/to/jedi_stuff
+# or remember to export RUBYOPT=rubygems
 
-require 'open-uri'
 require 'openils/mapper'
 require 'edi/edi2json'
 
-puts '../testdata/BakerAndTaylor_Samples/edifact_sample.flat.po'
-interchange = open('../testdata/BakerAndTaylor_Samples/edifact_sample.flat.po') { |io| EDI::E::Interchange.parse(io) }
-# interchange = open('http://github.com/senator/OpenILS-Evergreen/raw/5815302f00aa0ffed59210a0cfaed2d9927f64c6/edi_scratch/testdata/BakerAndTaylor_Samples/edifact_sample.flat.po') { |io| EDI::E::Interchange.parse(io) }
+a = ARGV
+a.push('../testdata/BakerAndTaylor_Samples/edifact_sample.flat.po') if a.size < 1
+a.each { |x|
+  interchange = open(x) { |io| EDI::E::Interchange.parse(io) }
+  puts x
+  puts interchange.to_json
+}
 
-puts interchange.to_json
 puts 'done'
