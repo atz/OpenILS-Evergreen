c39410ea952e77696dfa4afbfba22390c52d4da8
Joe Atzberger <atz@esilibrary.com>
2010-02-19 00:40:08 +0000

    Working as intended edi_pusher
    send_core changed to operate on id values instead of edi_message objects.
    The reason is that the objects themselves are out of date, having just
    been created and default DB values having been auto-populated "underneath"
    the object representation.  So we would have to fetch the objects again
    anyway.  Might as well be inside the sub.

--- Open-ILS/src/support-scripts/edi_pusher.pl
+++ Open-ILS/src/support-scripts/edi_pusher.pl
@@ -25,7 +25,6 @@ use OpenILS::Utils::Cronscript;
 use OpenILS::Utils::Fieldmapper;
 use OpenILS::Application::AppUtils;
 use OpenILS::Application::Acq::EDI;
-use OpenILS::Application::Acq::EDI::Translator;
 
 INIT {
     $debug = 1;
@@ -42,24 +41,23 @@ sub editor {
 my $e = editor();
 my $hook = 'format.po.jedi';
 my $defs = $e->search_action_trigger_event_definition({hook => $hook});
-
 # print Dumper($defs);
 print "\nHook '$hook' is used in ", scalar(@$defs), " event definition(s):\n";
 
 $Data::Dumper::Indent = 1;
-my $trans = OpenILS::Application::Acq::EDI::Translator->new();
 foreach my $def (@$defs) {
     printf "%3s - '%s'\n", $def->id, $def->name;
     my $events = $e->search_action_trigger_event([
         {event_def => $def->id},
         {flesh => 1, flesh_fields => { atev => ['template_output'] }}
     ]);
-    print "Event definition ", $def->id, " has ", scalar(@$events), " events\n";
+    print "Event definition ", $def->id, " has ", scalar(@$events), " event(s)\n";
     foreach (@$events) {
         my $message = Fieldmapper::acq::edi_message->new;
-        print "Event ", $_->id, " targets PO ", $_->target, ":\n";
+        $message->create_time('NOW');   # will need this later when we try to update from the object
+        print "Event ", $_->id, " targets PO ", $_->target, ":\n";  # target is an opaque identifier, so we cannot flesh it
         print Dumper($_), "\n";
-        my $target = $e->retrieve_acq_purchase_order([
+        my $target = $e->retrieve_acq_purchase_order([              # instead we retrieve it separately
             $_->target, {
                 flesh => 2,
                 flesh_fields => {
@@ -68,13 +66,12 @@ foreach my $def (@$defs) {
                 },
             }
         ]);
+
         print "Target: ", Dumper($target), "\n";
         my $logstr = sprintf "provider %s (%s)", $target->provider->id, $target->provider->name;
         unless ($target->provider->edi_default and $message->account($target->provider->edi_default->id)) {
             printf STDERR "ERROR: No edi_default account found for $logstr.  File will not be sent!\n";
         }
-        my $edi = $trans->json2edi($_->template_output()->data);   #  Going underneath EDI.pm
-        print "\n\ntranslator output: ", Dumper($edi), "\n";
         $message->jedi($_->template_output()->data);
         print "\ntarget->provider->edi_default->id: ", $target->provider->edi_default->id, "\n";
         print "\nNow calling attempt_translation\n\n";
@@ -84,14 +81,16 @@ foreach my $def (@$defs) {
             # The premise here is that if the translator failed, it is better to try again later from a "fresh" fetched file
             # than to add a cascade of failing inscrutable copies of the same message(s) to our DB.  
         }
-        print "Writing new message + translation in to DB\n";
+        print "Writing new message + translation to DB\n";
         $e->xact_begin;
         $e->create_acq_edi_message($message) or warn "create_acq_edi_message failed!  $!";
         $e->xact_commit;
 
         print "Calling send_core(...)\n";
-        if (OpenILS::Application::Acq::EDI->send_core($target->provider->edi_default, [$message])) {
-            print "\tSent message ", $message->id, "\n";
+        my $res = OpenILS::Application::Acq::EDI->send_core($target->provider->edi_default, [$message->id]);
+        if (@$res) {
+            my $message_out = shift @$res;
+            print "\tmessage ", $message->id, " status: ", $message_out->status, "\n";
         } else {
             print STDERR "ERROR: send_core failed for message ", $message->id, "\n";
         }
